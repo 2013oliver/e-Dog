e2f463d167fd9045c38f396316248298e200a856
2013oliver <90613804+2013oliver@users.noreply.github.com>
2023-01-26 18:35:10 +0800

    添加功能

--- src/Dog.java
+++ src/Dog.java
@@ -1,10 +1,12 @@
 public class Dog {
     public int Age;
-	public static int c = 0;
+	public static int c = 5;
     public static String Name;
     public boolean Is_sit;
     public boolean Is_bowl_empty = true;
     public static boolean Is_dog_full;
+	public int snack = 200;
+	public static int money = 100;
 
     public Dog(int age,String name){
         Age = age;
@@ -28,47 +30,63 @@ public class Dog {
 			this.x = x;
 		}
 		public void Ball(){
-			if (c >= 5) {
+			if (c == 0) {
 				if (Is_dog_full) {
-					System.out.println(Name + "玩累了,下次再玩吧!");
+					System.out.println(Name + "玩累了,给它吃点零食可以恢复两次玩的次数!");
 				} else {
 					System.out.println(Name + "玩累了,给它吃东西可以恢复能量!");
 				}
 			} else {
-				c++;
+				c--;
+				money += 2;
 				System.out.println("你和" + Name + "玩球," +  Name + "很开心!!");
+				System.out.println("游戏币+2");
 			}
 		}
 		public void Stuffed_Toy(){
-			if (c >= 5) {
+			if (c == 0) {
 				if (Is_dog_full) {
-					System.out.println(Name + "玩累了,下次再玩吧!");
+					System.out.println(Name + "玩累了,给它吃点零食可以恢复两次玩的次数!");
 				} else {
 					System.out.println(Name + "玩累了,给它吃东西可以恢复能量!");
 				}
 			} else {
-				c++;
+				c--;
+				money += 2;
 				System.out.println(Name + "在玩毛绒玩具," + Name + "很开心!!");
+				System.out.println("游戏币+2");
 			}
 		}
 	}
-    public void Eat(){
-        if (Is_bowl_empty) {
-            System.out.println("饭盆现在空了,你需要往饭盆里装满狗粮!!");
-        } else if (Is_dog_full){
-            System.out.println(Name + "已经吃饱了,再给它吃它会撑的!");
-        } else {
-            Is_bowl_empty = true;
-            System.out.println(Name + "吃饱了,它很开心.");
-            Is_dog_full = true;
-			c = 0;
-        }
+    public void Eat() {
+	    if (Is_bowl_empty) {
+		    System.out.println("饭盆现在空了,你需要往饭盆里装满狗粮!!");
+	    } else if (Is_dog_full) {
+		    System.out.println(Name + "已经吃饱了,再给它吃它会撑的!");
+	    } else {
+		    Is_bowl_empty = true;
+		    System.out.println(Name + "吃饱了,它很开心.");
+		    Is_dog_full = true;
+		    c += 5;
+		    money += 3;
+		    System.out.println("游戏币+3");
+	    }
     }
-
-
+	public void Eat_Snack() {
+		if (snack != 0) {
+			snack -= 10;
+			c += 2;
+			System.out.println(Name + "吃了一点零食,还剩" + snack / 100 + "盒零食!");
+			money ++;
+			System.out.println("游戏币+1");
+		} else {
+			System.out.println("没零食了,去商店买一点零食吧!");
+		}
+	}
     public void Print_Dog(){
         System.out.println("名字:" + Name);
         System.out.println("年龄:" + Age);
+		System.out.println("游戏币:" + money);
         if (Is_sit){
             System.out.println(Name + "现在是坐的状态");
         } else {
@@ -84,5 +102,6 @@ public class Dog {
         } else {
             System.out.println("饭碗里是满的");
         }
+	    System.out.println("还有" + snack / 100 + "盒零食!");
     }
 }
--- src/Dog_test.java
+++ src/Dog_test.java
@@ -6,10 +6,10 @@ public class Dog_test {
 		Dog my_dog = new Dog(1,"a");
 		int n;
 		System.out.println("欢迎来到狗狗模拟器!");
-		System.out.println("[1]输入或更新狗狗的个人信息 [2]让狗狗坐 [3]让狗狗站 [4]往饭盆里装满狗粮 [5]让狗狗吃饭 [6]和狗狗玩 [7]输出狗狗个人信息 [8]退出");
+		System.out.println("[1]输入或更新狗狗的个人信息 [2]让狗狗坐 [3]让狗狗站 [4]往饭盆里装满狗粮 [5]让狗狗吃饭 [6]和狗狗玩 [7]喂狗狗零食 [8]商店 [9]输出狗狗个人信息 [10]退出");
 		System.out.print("请输入你需要的操作:");
 		n = cin.nextInt();
-		while (n != 8){
+		while (n != 10){
 			if (n == 1){
 				System.out.print("请输入狗狗的名字:");
 				String name = cin.next();
@@ -25,7 +25,7 @@ public class Dog_test {
 				my_dog.Fill_Bowl();
 			} else if (n == 5) {
 				my_dog.Eat();
-			} else if (n == 7) {
+			} else if (n == 9) {
 				my_dog.Print_Dog();
 			} else if (n == 6) {
 				System.out.println("玩具如下:");
@@ -42,6 +42,29 @@ public class Dog_test {
 					}
 					System.out.print("请输入需要和" + my_dog.Name + "玩的玩具:");
 					m = cin.nextInt();
+				}
+			} else if (n == 7) {
+				my_dog.Eat_Snack();
+			} else if (n == 8) {
+				System.out.println("=========商店=========");
+				System.out.println("物品列表:");
+				System.out.println("[1]零食(10游戏币/盒) [2]退出");
+				System.out.print("请输入你需要购买的商品:");
+				int u;
+				u = cin.nextInt();
+				while (u != 2){
+					if (u == 1) {
+						System.out.print("请输入盒数:");
+						int t = cin.nextInt();
+						if (t * 10 > my_dog.money) {
+							System.out.println("余额不足!!");
+						} else {
+							my_dog.money -= t * 10;
+							System.out.println("成功购买.");
+						}
+					}
+					System.out.print("请输入你需要购买的商品:");
+					u = cin.nextInt();
 				}
 			}
 			System.out.print("请输入你需要的操作:");
